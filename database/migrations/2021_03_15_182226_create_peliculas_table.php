ce85e082e11fe18aaa4556fd51a5c40606a42fc9
Ignacio Coria <ignaciocoriadb@gmail.com>
2021-03-15 15:26:15 -0300

    migration peliculas , directores

---
+++ database/migrations/2021_03_15_182226_create_peliculas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->string("titulo");
+            $table->date("anio");
+            $table->string("imagen_portada");
+            $table->unsignedBigInteger("director_id");
+            $table->unsignedBigInteger("actor_id");
+            $table->unsignedBigInteger("genero_id");
+            $table->unsignedBigInteger("user_id");
+            $table->timestamps();
+
+            //claves foraneas
+            $table->foreign('director_id')->references('id')->on('directores');
+            $table->foreign('actor_id')->references('id')->on('actores');
+            $table->foreign('genero_id')->references('id')->on('generos');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelicuphp alas');
+    }
+}
