ae678dfb2f0af8c9ccda812aea9f1d68eb31f00d
Ignacio Coria <ignaciocoriadb@gmail.com>
2021-03-18 15:02:13 -0300

    ruta directores

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('peliculas','App\Http\Controllers\PeliculasController');
+Route::resource('directores','App\Http\Controllers\DirectoresController');
